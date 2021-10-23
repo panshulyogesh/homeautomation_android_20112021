2c434f47eec3db471085fae50c53bf7bbac44ab3
panshul yogesh <panshulyogesh@gmail.com>
2021-10-23 10:18:07 +0530

    version1

---
+++ App.js
@@ -0,0 +1,351 @@
+/*
+ *    REACT NATIVE  HOME AUTOMATION
+ *    ANDROID CLIENT
+ *    WRITTEN BY SNEHAL SANTOSH VELANKAR
+ *
+ */
+
+import React, {useEffect} from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  Image,
+} from 'react-native';
+
+import {NavigationContainer} from '@react-navigation/native';
+
+import {createStackNavigator} from '@react-navigation/stack';
+
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+import FirstPage from './pages/FirstPage';
+
+import SecondPage from './pages/SecondPage';
+
+import datacq from './pages/datacq';
+
+import OwnerRegistration from './pages/OwnerRegistration';
+
+import ApplianceRegistration from './pages/ApplianceRegistration';
+
+import LocationRegistration from './pages/LocationRegistration';
+
+import Binding from './pages/Binding';
+
+import Pairing from './pages/Pairing';
+
+import acqreg from './pages/acqreg';
+
+import ModifyOwner from './pages/ModifyOwner';
+
+import DummyScreen from './pages/DummyScreen';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+/////////////////////
+//! REACT NATIVE SQLITE DATABASE CONNECTION
+//! DATABASE NAME = UserDatabase.db
+/////////////////////////
+import {openDatabase} from 'react-native-sqlite-storage';
+var db = openDatabase({name: 'UserDatabase.db'});
+
+function TabStack() {
+  return (
+    //!  DECLARING CONTROLLER SCREEN
+
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
+
+          if (route.name === 'FirstPage') {
+            iconName = focused ? 'person' : 'person';
+          } else if (route.name === 'datacq') {
+            iconName = focused ? 'recording' : 'recording';
+          } else if (route.name === 'SecondPage') {
+            iconName = focused ? 'settings' : 'settings';
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
+      })}
+      initialRouteName="Controller">
+      <Tab.Screen
+        name="FirstPage"
+        tabBarLabel="controller"
+        component={FirstPage}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="datacq"
+        tabBarLabel="datacq"
+        component={datacq}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="SecondPage"
+        tabBarLabel="registration"
+        component={SecondPage}
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+const App = () => {
+  //! As soon as application is installed useeffect is called to create database tables
+  useEffect(() => {
+    //!  owner ref table is for registering owner
+    db.transaction(function (txn) {
+      txn.executeSql(
+        "SELECT name FROM sqlite_master WHERE type='table' AND name='Owner_Reg'",
+        [],
+        function (tx, res) {
+          if (res.rows.length == 0) {
+            txn.executeSql('DROP TABLE IF EXISTS Owner_Reg', []);
+            txn.executeSql(
+              `CREATE TABLE IF NOT EXISTS Owner_Reg(
+              Id INTEGER PRIMARY KEY AUTOINCREMENT,
+              owner_name TEXT,
+              owner_password TEXT,
+              MailId TEXT,
+              PhoneNumber INT(15),
+              Property_name TEXT, 
+              Area TEXT,
+              State TEXT,
+              pincode TEXT,
+              Street TEXT,
+              Door_Number  TEXT,router_ssid TEXT,router_password TEXT,DAQ_STACTIC_IP TEXT, DAQ_STACTIC_Port TEXT )`,
+              [],
+            );
+          }
+        },
+      );
+    });
+
+    //! location registration table for registering locations like hall , kitchen ,bedroom etc
+    db.transaction(function (txn) {
+      txn.executeSql(
+        "SELECT name FROM sqlite_master WHERE type='table' AND name='Location_Reg'",
+        [],
+        function (tx, res) {
+          if (res.rows.length == 0) {
+            txn.executeSql('DROP TABLE IF EXISTS Location_Reg', []);
+            txn.executeSql(
+              `CREATE TABLE IF NOT EXISTS Location_Reg(Location TEXT PRIMARY KEY,images TEXT)`,
+              [],
+            );
+          }
+        },
+        function (tx, res) {
+          console.log(error);
+        },
+      );
+    });
+    //! Appliance reg table for registering appliance like tv,fan,ac, light etc....
+    db.transaction(function (txn) {
+      txn.executeSql(
+        "SELECT name FROM sqlite_master WHERE type='table' AND name='Appliance_Reg'",
+        [],
+        function (tx, res) {
+          if (res.rows.length == 0) {
+            txn.executeSql('DROP TABLE IF EXISTS Appliance_Reg', []);
+            txn.executeSql(
+              `CREATE TABLE IF NOT EXISTS Appliance_Reg(Appliance TEXT PRIMARY KEY,binded_unbinded TEXT)`,
+              [],
+            );
+          }
+        },
+      );
+    });
+
+    //column name = LOC, APPLIANCE,MODEL,PAIRED/UNPAIRED,>> IF PAIRED MACID,PROPERTIES,status
+    //binding str=owner+loc+appli+model;
+
+    //! Binding reg table is for binding each location , appliance with their specific models,properties,ip addresses etc
+    db.transaction(function (txn) {
+      txn.executeSql(
+        "SELECT name FROM sqlite_master WHERE type='table' AND name='Binding_Reg'",
+        [],
+        function (tx, res) {
+          if (res.rows.length == 0) {
+            txn.executeSql('DROP TABLE IF EXISTS Binding_Reg', []);
+            txn.executeSql(
+              `CREATE TABLE IF NOT EXISTS Binding_Reg(location TEXT,
+              appliance TEXT, model TEXT,paired_unpaired TEXT,
+              ipaddress TEXT,macid TEXT,portnumber TEXT,wifi_ssid TEXT,wifi_pwd TEXT,device_type TEXT,actions TEXT,
+              properties TEXT,Control_function TEXT,pin_direction TEXT,Valid_States TEXT,output TEXT,lan TEXT,wan TEXT,
+              ACS_controller_model TEXT,ESP_pin TEXT,status TEXT,color TEXT)`,
+              [],
+            );
+          }
+        },
+      );
+    });
+
+    //!  models list table is to store appliance models properties which is a global reserve and is captured from a serveer
+    db.transaction(function (txn) {
+      txn.executeSql(
+        "SELECT name FROM sqlite_master WHERE type='table' AND name='models_list'",
+        [],
+        function (tx, res) {
+          if (res.rows.length == 0) {
+            txn.executeSql('DROP TABLE IF EXISTS models_list', []);
+            txn.executeSql(
+              `CREATE TABLE IF NOT EXISTS models_list(Model TEXT,device_type TEXT ,Properties TEXT,Control_function TEXT,pin_direction TEXT,
+              Valid_States TEXT,output TEXT,ACS_controller_model TEXT,ESP_pin TEXT,actions TEXT )`,
+              [],
+            );
+          }
+        },
+      );
+    });
+
+    db.transaction(function (txn) {
+      txn.executeSql(
+        "SELECT name FROM sqlite_master WHERE type='table' AND name='Data_Acquisition '",
+        [],
+        function (tx, res) {
+          if (res.rows.length == 0) {
+            txn.executeSql('DROP TABLE IF EXISTS Data_Acquisition', []);
+            txn.executeSql(
+              `CREATE TABLE IF NOT EXISTS Data_Acquisition(coordinates TEXT ,timestamp TEXT,bindingid TEXT,value TEXT)`,
+              [],
+            );
+          }
+        },
+      );
+    });
+    db.transaction(function (txn) {
+      txn.executeSql(
+        "SELECT name FROM sqlite_master WHERE type='table' AND name='Data_Acq_master'",
+        [],
+        function (tx, res) {
+          if (res.rows.length == 0) {
+            txn.executeSql('DROP TABLE IF EXISTS Data_Acq_master', []);
+            txn.executeSql(
+              `CREATE TABLE IF NOT EXISTS Data_Acq_master(daqip TEXT ,daqport TEXT,wifi_ssid TEXT,wifi_pwd TEXT)`,
+              [],
+            );
+          }
+        },
+      );
+    });
+    db.transaction(function (txn) {
+      txn.executeSql(
+        "SELECT name FROM sqlite_master WHERE type='table' AND name='device_count'",
+        [],
+        function (tx, res) {
+          if (res.rows.length == 0) {
+            txn.executeSql('DROP TABLE IF EXISTS device_count', []);
+            txn.executeSql(
+              `CREATE TABLE IF NOT EXISTS device_count(device TEXT PRIMARY KEY  ,count TEXT)`,
+              [],
+            );
+          }
+        },
+      );
+    });
+  }, []);
+  //http://192.168.1.101:8085/26/$NETWORK/LAN;abcdefgh;123456;%
+  // http://192.168.1.101:8085/$NETWORK/LAN;abcdefgh;123456;%
+  return (
+    <NavigationContainer>
+      <Image source={require('./logo.png')} />
+      <Stack.Navigator
+        initialRouteName="Controller"
+        screenOptions={{
+          headerStyle: {backgroundColor: `#008080`},
+          headerTintColor: '#fff',
+          headerTitleStyle: {fontWeight: 'bold'},
+        }}>
+        <Stack.Screen
+          name="TabStack"
+          component={TabStack}
+          options={{
+            headerShown: false,
+          }}
+        />
+
+        <Stack.Screen
+          name="OwnerRegistration"
+          component={OwnerRegistration}
+          options={{
+            tabBarLabel: 'Owner Registration',
+          }}
+        />
+
+        <Stack.Screen
+          name="ModifyOwner"
+          component={ModifyOwner}
+          options={{
+            tabBarLabel: ' Edit Owner Registration',
+          }}
+        />
+        <Stack.Screen
+          name="DummyScreen"
+          component={DummyScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="ApplianceRegistration"
+          component={ApplianceRegistration}
+          options={{
+            tabBarLabel: 'Appliance Registration',
+          }}
+        />
+
+        <Stack.Screen
+          name="LocationRegistration"
+          component={LocationRegistration}
+          options={{
+            tabBarLabel: 'Location Registration',
+          }}
+        />
+
+        <Stack.Screen
+          name="Binding"
+          component={Binding}
+          options={{
+            tabBarLabel: 'Binding',
+          }}
+        />
+        <Stack.Screen
+          name="Pairing"
+          component={Pairing}
+          options={{
+            tabBarLabel: 'Pairing',
+          }}
+        />
+
+        <Stack.Screen
+          name="acqreg"
+          component={acqreg}
+          options={{
+            tabBarLabel: 'acqreg',
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+export default App;
